7fc7a5d446181cc1c0eded975f35c894fd98a3a8
melnichenkosa <melnichenkosa@411D-5>
2023-03-09 11:56:41 +0700

    Добавьте файлы проекта.

---
+++ PolischNotation/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolischNotation
+{
+    internal sealed class Node<T>
+    {
+        /// <summary>
+        /// Возвращает значение, содержащееся в узле.
+        /// </summary>
+        internal T? Data { get; }
+
+        /// <summary>
+        /// Получает и устанавливает ссылку на связный узел.
+        /// </summary>
+        internal Node<T>? Link { get; set; }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса LinkedNode, содержащего указанное значение.
+        /// </summary>
+        /// <param name="data">Значение, которое должно содержаться в списке.</param>
+        internal Node(T? data = default) => Data = data;
+    }
+}
---
+++ PolischNotation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PolischNotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            calculator.calculator();
+        }
+    }
+}
